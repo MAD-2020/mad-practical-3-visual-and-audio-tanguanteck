6d19d021de711a87834117d91711acfb8890b37c
Guan Teck Tan <s10198165@connect.np.edu.sg>
2020-05-19 19:05:20 +0800

    week 3 Practical

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,10 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MediaPlayer audioBaby = MediaPlayer.create(this, R.raw.baby_laughing);
+        audioBaby.start();
+
+        VideoView rabbidVideo = findViewById(R.id.videoView2);
+        rabbidVideo.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() +  "/" +R.raw.rabbid)
+        );
+        rabbidVideo.start();
     }
 }
